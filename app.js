11bff1224cf692928eef67a732272a75dae8eb2c
V Krishnakumar <krishkumarv2001@gmail.com>
2021-07-15 21:44:12 +0530

    finished app

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const { render } = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-
+const blogRoutes = require("./routes/blogRoutes");
 //Initialize Express
 const app = express();
 
@@ -34,43 +33,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 //mongoose and mongo sandbox routes
-// app.get("/add-blog", (req, res) => {
-//   const blog = new Blog({
-//     title: "new Blog 2",
-//     snippet: "About my new blog v2",
-//     body: "More about my new blog 2",
-//   });
-
-//   blog
-//     .save()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/all-blogs", (req, res) => {
-//   Blog.find()
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get("/single-blog", (req, res) => {
-//   Blog.findById("60ed3dca1a96e02b5cc60b76")
-//     .then((result) => {
-//       res.send(result);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
@@ -79,57 +41,11 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
 app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
 
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new Blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => console.log(err));
-});
+app.use("/blogs", blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
